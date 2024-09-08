d1440a13d5ad759ec34f1b03a5d7fcc7790cf4d9
Ancete <gustavoaancete@gmail.com>
2024-09-08 19:11:54 -0300

    continuação das aulas

--- aula15/ex.js
+++ aula15/ex.js
@@ -2,9 +2,9 @@ let num1;
 
 num1 = parseInt(prompt("Informe um número:"))
 console.log(`Raiz quadrada: ${num1 ** (1/2)}`)
-console.log(`Numero é inteiro: ${}`)
-console.log(`Raiz quadrada: ${}`)
-console.log(`Raiz quadrada: ${Math.floor(num1)}`)
-console.log(`Raiz quadrada: ${Math.ceil(num1)}`)
-console.log(`Raiz quadrada: ${Math.}`)
+console.log(`Numero é inteiro: ${Number.isInteger(num1)}`)
+console.log(`É NaN: ${Number.isNaN(num1)}`)
+console.log(`Arredondado para baixo: ${Math.floor(num1)}`)
+console.log(`Arredondado para cima: ${Math.ceil(num1)}`)
+console.log(`Com duas casas decimais: ${num1.toFixed(2)}`)
 
---
+++ aula16/index.js
@@ -0,0 +1,17 @@
+//                0      1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+//alunos.pop(); remove o ultimo elemento
+//aluno.shift(); remove o primeiro elemento
+
+//alunos.unshift('Luiza'); Adiciona no começo
+
+//alunos.push('Otávio'); adiciona no fim
+//alunos[alunos.length] = 'Luiza' - adiciona no fim
+
+//alunos[0] = 'Eduardo';
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
+//alunos[3] = 'Pedro';
+//console.log(alunos);
